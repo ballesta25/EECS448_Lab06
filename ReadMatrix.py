085e291c9d11e8a1d7d787699f8c00379f17f92a
Jeffrey Kraus <jkraus@1005d-2.eecs.ku.edu>
2015-03-23 18:04:42 -0500

    Updated to return matrix dimensions and allow formerly invalid csv syntax

--- ReadMatrix.py
+++ ReadMatrix.py
@@ -1,20 +1,20 @@
 def readMatrix(filename):
     matFile = open(filename, 'r')
     mat = []
-    try:
-        for line in matFile:
-            newRow = line.split(',')
-            for x in range(0, len(newRow)):
+    for line in matFile:
+        print(line)
+        newRow = line.split(',')
+        print(newRow)
+        for x in range(0, len(newRow)):
+            if is_number(newRow[x]):            
                 newRow[x] = float(newRow[x])
-            mat.append(newRow)
-    except:
-        mat = 0
-    if isMatrix(mat):
-        return mat
-    else:
-        return 0
+            else:
+                newRow.remove(newRow[x])
+        print newRow
+        mat.append(newRow)
+    return mat
 
-def isMatrix(a):
+def getDimensions(a):
     try:
         height = len(a)
         width = len(a[0])
@@ -23,6 +23,13 @@ def isMatrix(a):
         for row in range(0, height):
             if len(a[row]) != width:
                 return False
+        return (height, width)
+    except:
+        return False
+
+def is_number(s):
+    try:
+        float(s)
         return True
     except:
         return False
@@ -32,13 +39,18 @@ def main():
         matrixA = readMatrix("matrixA.csv")
     except:
         print("Error: invalid file")
-    
-    if matrixA == 0:
+    try:
+        dim = getDimensions(matrixA)
+        if dim:
+            print("Dimensions:")
+            print(str(getDimensions(matrixA)) + '\n')
+            print("Matrix:")
+            for row in range(0, len(matrixA)):
+                print(matrixA[row])
+        else:
+            print("Error: invalid matrix")
+    except:
         print("Error: invalid matrix")
-    else:
-        print("Matrix:")
-        for row in range(0, len(matrixA)):
-            print(matrixA[row])
     return
 
 if __name__ == "__main__":
